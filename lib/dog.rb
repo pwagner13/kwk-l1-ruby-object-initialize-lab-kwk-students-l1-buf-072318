9d423bfc73dfcb4495018844577c1c127b2d97ee
pwagner13 <pwagner@buffaloseminary.org>
2018-07-27 19:42:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,9 @@
 # dog.rb
+
+class Dog 
+  attr_reader :name, :breed
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,10 @@
 # person.rb
+
+class Person 
+  attr_reader :name
+  def initialize(name)
+  @name = name
+  end
+end
+
+
